6cb7b3ab4cd2eb6a3abccdde900376d677d78c48
Oytunkdnz <oytunakdeniz@gmail.com>
2020-07-11 09:14:26 +0300

    Tek Nesne Tasarım Kalıbı

---
+++ src/creational/singleton/Singleton.java
@@ -0,0 +1,27 @@
+package creational.singleton;
+
+public class Singleton {
+	private static Singleton singleton;
+	
+	private static int sayi = 0;
+	private Singleton() {
+		System.out.println("ben oluştum.");
+	}
+
+	public static Singleton getSingleton() {
+		if (singleton == null)
+			synchronized(Singleton.class) {
+				if (singleton == null)
+					singleton = new Singleton();
+			}
+			
+		++sayi;
+		System.out.println(sayi);
+		
+		return singleton;
+	}
+
+	
+	
+	
+}
